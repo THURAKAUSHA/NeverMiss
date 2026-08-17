2281184ff70e405866e7a63f8e165bcc9b821875
TwinMind Developer <admin@twinmind.dev>
2026-08-17 18:42:38 +0530

    Use supported Llama model

--- nevermiss.py
+++ nevermiss.py
@@ -216,7 +216,7 @@ Email:
 
     try:
         response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {
                     "role": "system",
